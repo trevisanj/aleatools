56137c73444501d8124e236a4ac466d7dd524eb2
Logan da Silva <juliotrevisan@gmail.com>
2020-02-28 11:15:06 -0300

    as

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-import sys
-# if sys.version_info[0] < 3:
-#     print("Python version detected:\n*****\n{0!s}\n*****\nCannot run, must be using Python 3".format(sys.version))
-#     sys.exit()
-
 from setuptools import setup, find_packages
 from glob import glob
 
@@ -10,7 +5,7 @@ setup(
     name = 'aleatools',
     packages = find_packages(),
     include_package_data=True,
-    version = '2019.1.7',
+    version = '20.02.23.0',
     license = 'GNU GPLv3',
     platforms = 'any',
     description = 'Aleatory Tools',
@@ -18,7 +13,7 @@ setup(
     author_email = 'juliotrevisan@gmail.com',
     url = 'http://github.com/trevisanj/aleatools',
     keywords= [],
-    install_requires = ['a107>=18.1.1.0', 'pycrypto'],
+    install_requires = ['a107>=20.02.23.0', 'pycrypto'],
     python_requires = '>=3',
     scripts = glob('aleatools/scripts/*.py')
 )
