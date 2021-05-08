321e169b961b699061552b180bc6fdc7cc10e093
Logan <juliotrevisan@gmail.com>
2021-05-08 12:41:31 -0300

    addcommitpush

---
+++ aleatools/scripts/addcommitpush.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os, sys, argparse, a107
+
+SIMULATION = False
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please specify a commit message as one or more command-line arguments.")
+        sys.exit()
+    msg = " ".join(sys.argv[1:])
+    f = print if SIMULATION else os.system
+    f("git add . --all")
+    f("git commit -m \"{}\"".format(msg))
+    f("git push")
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name = 'aleatools',
     packages = find_packages(),
     include_package_data=True,
-    version = '20.02.23.0',
+    version = '20.05.08.0',
     license = 'GNU GPLv3',
     platforms = 'any',
     description = 'Aleatory Tools',
@@ -13,7 +13,7 @@ setup(
     author_email = 'juliotrevisan@gmail.com',
     url = 'http://github.com/trevisanj/aleatools',
     keywords= [],
-    install_requires = ['a107>=20.02.23.0', 'pycrypto'],
+    install_requires = ['pycrypto'],
     python_requires = '>=3',
     scripts = glob('aleatools/scripts/*.py')
 )
