4246b61516235b2d7b7d49199ab4319dbe822119
Logan <juliotrevisan@gmail.com>
2021-05-08 12:42:04 -0300

    addcommitpush

--- aleatools/scripts/addcommitpush.py
+++ aleatools/scripts/addcommitpush.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
         print("Please specify a commit message as one or more command-line arguments.")
         sys.exit()
     msg = " ".join(sys.argv[1:])
+    print(f"Commit message: {msg}")
     f = print if SIMULATION else os.system
     f("git add . --all")
     f("git commit -m \"{}\"".format(msg))
